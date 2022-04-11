193ab8105f272b0ab4d396a4284e82dd30245ae6
ciboq <90256806+ciboq@users.noreply.github.com>
2022-04-11 18:06:57 -0400

    m

--- script.js
+++ script.js
@@ -5,36 +5,63 @@ fetch('https://api.airtable.com/v0/appwNEs14ZXwbbZ0B/Table%201', {
 })
   .then(res => res.json()) 
   .then(data => {
-    console.log(data); 
+    arr=data.records.filter(board => board.fields.snowboard_brand === 'Burton')
+  console.log(arr)
+    let board1 = arr.slice(-5)
+    let board2 = arr.slice(0,-4)
+    
+    var progress = document.querySelector('.progress');
 
-    const snowboard_container = document.querySelector('.snowboard_container'); 
-    data.records.forEach(album => {
+    const snowboard_container_burton1 = document.querySelector('.one');
+    const snowboard_container_burton2 = document.querySelector('.two');
+
+    data.records
+    
+    .forEach((album, index) => {
       console.log(album); 
+    
+  
+  snowboard_container_burton1.innerHTML += `
 
-      
-snowboard_container.innerHTML += `
+  <div class="swiper-slide one">
+  <div><img class="snowboard" src="${album.fields.snowboard_pic[0].thumbnails.large.url}", /></div>
+  </div>
+  
+  `;
+
+
+snowboard_container_burton2.innerHTML += `
+
+<div class="swiper-slide two">
+<div><img class="snowboard" src="${album.fields.snowboard_pic[0].thumbnails.large.url}"/></div>
+</div>
 
 `;
-    });
+
+
+
     
-  var progress = document.querySelector('.progress');
 
-  var myAnimation = anime({
-    targets: 'img',
-    translateX: '500px',
-    duration: 2000,
-    easing: 'easeInOutExpo',
-    loop: true,
-    direction: 'alternate',
-    update: function(animation) {
-      progress.value = animation.progress;
-    }
+anime({
+  targets: '.snowboard',
+  translateX: '220px',
+  duration: function() { return anime.random(1000,1400); },
+  delay: function() { return anime.random(300,600); },
+  easing: 'easeInOutExpo',
+  loop: true,
+  direction: 'alternate',
+  update: function(animation) {
+    progress.value = animation.progress;
+  }
+});
+
+});
+
   });
 
-progress.addEventListener('input', function() { myAnimation.seek(progress.value); });
 
-  
-  });
+
+    
   
 
 // let snowboardContainer = document.querySelector(".snowboard_container");
